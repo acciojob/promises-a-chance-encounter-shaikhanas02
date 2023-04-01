f16519d508fe8421e6ff3cf8c64fdb3677ae21c5
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-04-02 00:11:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+// Function to generate a random number between 1 and 10
+  function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
+  // Function to create a promise that resolves to a random number or rejects with an error
+  function createPromise() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (Math.random() < 0.5) {
+          resolve(getRandomNumber());
+        } else {
+          reject(new Error('Promise rejected with error'));
+        }
+      }, 1000);
+    });
+  }
+
+  // Create an array of 5 promises
+  const promises = [
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise(),
+    createPromise()
+  ];
+
+  // Use Promise.all to wait for all promises to settle and log the results or errors
+  Promise.all(promises)
+    .then(results => {
+      results.forEach((result, index) => {
+        if (result instanceof Error) {
+          console.log(`Promise ${index + 1} rejected with error`);
+        } else {
+          const outputDiv = document.getElementById('output');
+          const p = document.createElement('p');
+          p.textContent = `Promise ${index + 1}: ${result}`;
+          outputDiv.appendChild(p);
+        }
+      });
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
